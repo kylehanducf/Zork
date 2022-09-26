57251424b0cff4475fe7c678d79b458555f4bb74
kylehanducf <kyle.hand.kh@gmail.com>
2022-09-26 15:36:36 -0400

    3.1 Initial Commit (Room Class & Room Initialization)

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
 
 namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -17,7 +15,7 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
@@ -29,7 +27,7 @@ namespace Zork
                 switch (command)
                 {
                     case Commands.LOOK:
-                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door");
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
@@ -54,15 +52,11 @@ namespace Zork
                         Console.WriteLine("Unknown Command");
                         break;
                 }
-
             }
-
-
         }
 
         private static bool Move(Commands command)
         {
-
             bool isValidMove = true;
             switch (command)
             {
@@ -92,12 +86,27 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
-        private static readonly string[,] Rooms =
-        { 
-            {"Rocky Trail", "South of House", "Canyon View"},
-            {"Forest", "West of House", "Behind House"},
-            {"Dense Woods", "North of House", "Clearing"}
+        private static readonly Room[,] Rooms =
+        {
+            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
+        
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        }
 
         private static (int Row, int Column) Location = (1, 1);
 
