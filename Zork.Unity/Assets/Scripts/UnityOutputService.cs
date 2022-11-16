c7e6cde1d7784d103c644f504506dce1f16c382b
kylehanducf <55325367+kylehanducf@users.noreply.github.com>
2022-11-16 13:36:48 -0500

    Class Changes

---
+++ Zork.Unity/Assets/Scripts/UnityOutputService.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Zork.Common;
+using TMPro;
+
+public class UnityOutputService : MonoBehaviour, IOutputService
+{
+    [SerializeField]
+    private TextMeshProUGUI TextLine;
+
+    public void Write(object obj)
+    {
+        TextLine.text = obj.ToString();
+    }
+
+    public void Write(string message)
+    {
+        TextLine.text = message.ToString();
+    }
+
+    public void WriteLine(object obj)
+    {
+        TextLine.text = obj.ToString();
+    }
+
+    public void WriteLine(string message)
+    {
+        TextLine.text = message.ToString();
+    }
+}
